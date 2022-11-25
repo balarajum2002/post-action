d586f8caecf788c720036110699127adaf489dca
balarajum2002 <mekalabalaraju777@gmail.com>
2022-11-25 11:53:49 +0530

    adding data

--- index.js
+++ index.js
@@ -14,5 +14,8 @@ app.use(cors())
 
 app.post("/newData",(req,res)=>{
 	console.log(req.body)
+	const {name,password,course,gender}=req.body
+	console.log(name,password,course,gender,"is the solution")
+
 })
 app.listen(port,()=>console.log("server is started"))
